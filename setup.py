f0ce6e0d7b5e647ebec1f162f81c82d2042cf2dd
v.moriceau <v.moriceau@cube-creative.com>
2018-04-05 15:30:29 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -3,38 +3,40 @@ from codecs import open
 from setuptools import setup, find_packages
 
 
-NAME = 'frangidoc'
-VERSION = '1.1.2'
-DESCRIPTION = 'Generate markdown from Python sources (reST based)'
-AUTHOR = 'Valentin Moriceau'
-AUTHOR_EMAIL = 'valentin.moriceau@free.com'
+_NAME = 'frangidoc'
+_VERSION = '1.1.3'
+_DESCRIPTION = 'Generate markdown from Python sources (reST based)'
+_URL = 'https://github.com/MrFrangipane/frangidoc'
+_AUTHOR = 'Valentin Moriceau'
+_AUTHOR_EMAIL = 'valentin.moriceau@free.com'
 
 _here = path.abspath(path.dirname(__file__))
-_readme_filepath = path.join(_here, 'README.md')
-_requirements_filepath = path.join(_here, 'requirements.txt')
 
+with open(path.join(_here, 'readme.md'), encoding='utf-8') as readme_file:
+    long_description = readme_file.read()
 
-if path.isfile(_readme_filepath):
-    with open(_readme_filepath, encoding='utf-8') as readme_file:
-        _long_description = readme_file.read()
-else:
-    _long_description = 'Unable to load README.md'
+with open(path.join(_here, "requirements.txt")) as requirements_file:
+    requirements = requirements_file.readlines()
 
+dependency_links = []
+install_requires = []
 
-if path.isfile(_requirements_filepath):
-    with open(_requirements_filepath) as requirements_file:
-        _requirements = requirements_file.readlines()
-else:
-    _requirements = list()
-
+for item in requirements:
+    if item.startswith('git+'):
+        dependency_links.append(str(item))
+        install_requires.append(item.split("/")[-1].split(".git")[0])
+    else:
+        install_requires.append(str(item))
 
 setup(
-    name=NAME,
-    version=VERSION,
-    description=DESCRIPTION,
-    author=AUTHOR,
-    author_email=AUTHOR_EMAIL,
+    name=_NAME,
+    version=_VERSION,
+    description=_DESCRIPTION,
+    url=_URL,
+    author=_AUTHOR,
+    author_email=_AUTHOR_EMAIL,
     packages=find_packages(exclude=['tests']),
-    install_requires=_requirements,
+    install_requires=install_requires,
+    dependency_links=dependency_links,
     include_package_data=True
 )
