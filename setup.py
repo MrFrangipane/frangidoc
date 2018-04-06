9a913f4e21891b919931883ec2a3080474d92682
v.moriceau <v.moriceau@cube-creative.com>
2018-04-06 12:30:04 +0200

    Deals with __main__.py files by running "-h" command

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 _NAME = 'frangidoc'
-_VERSION = '1.1.7'
+_VERSION = '1.2.0'
 _DESCRIPTION = 'Generate markdown from Python sources (reST based)'
 _URL = 'https://github.com/MrFrangipane/frangidoc'
 _AUTHOR = 'Valentin Moriceau'
